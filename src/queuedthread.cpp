0558445b052bdbcc4ee88a858c5551508db0d18e
Ithier Débauché <ithier.debauche@etu.univ-lyon1.fr>
2025-06-30 18:58:07 +0200

    forgot to change .cpp

--- src/queuedthread.cpp
+++ src/queuedthread.cpp
@@ -18,26 +18,6 @@ QueuedThread::~QueuedThread()
     }
 }
 
-template <typename T, std::enable_if<std::is_invocable_r_v<void, T>>>
-void QueuedThread::invoke(T&& task)
-{
-    {
-        std::lock_guard<std::mutex> lock(_mutex);
-        _deque.push_front(std::function<void(void)>(std::forward<T>(task)));
-    }
-    _condVar.notify_one();
-}
-
-template <typename T, std::enable_if<std::is_invocable_r_v<void, T>>>
-void QueuedThread::schedule(T&& task)
-{
-    {
-        std::lock_guard<std::mutex> lock(_mutex);
-        _deque.push_back(std::function<void(void)>(std::forward<T>(task)));
-    }
-    _condVar.notify_one();
-}
-
 void QueuedThread::run()
 {
     std::function<void(void)> task;
